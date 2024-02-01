9f74fcd3c768e9857111eef3ba773e0a4cad51b2
Luxky <taotim.taozaza@gmail.com>
2024-02-02 01:03:03 +0900

    add CharacterData Struct

---
+++ DareFightingICE-Unity/Assets/Script/Struct/CharacterData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script.Struct
+{
+    [System.Serializable]
+    public struct CharacterData
+    {
+        public bool playerNumber;
+        public int hp;
+        public int energy;
+        public Vector2 position;
+        public Rect bounds;
+        public Vector2 speed;
+        public State state;
+        public bool front;
+        public bool control;
+        public int remainingFrame;
+        public bool hitConfirm;
+        public int hitCount;
+        public int lastHitFrame;
+        public List<string> inputCommands;
+        public List<string> processedCommands;
+
+
+        public CharacterData(CharacterData characterData)
+        {
+            playerNumber = characterData.playerNumber;
+            hp = characterData.hp;
+            energy = characterData.energy;
+            position = characterData.position;
+            bounds = characterData.bounds;
+            speed = characterData.speed;
+            state = characterData.state;
+            front = characterData.front;
+            control = characterData.control;
+            remainingFrame = characterData.remainingFrame;
+            hitConfirm = characterData.hitConfirm;
+            hitCount = characterData.hitCount;
+            lastHitFrame = characterData.lastHitFrame;
+            inputCommands = new List<string>(characterData.inputCommands);
+            processedCommands = new List<string>(characterData.processedCommands);
+
+        }
+    }
+}
